6532126bb9cd3eb8e016f481c3a537d06f2176cc
Mouli <mouliofficial98@gmail.com>
2020-11-28 15:31:41 +0530

    signup updated

---
+++ signup.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function(){
+    var google = document.getElementById("google")
+    google.addEventListener("click",() =>{
+        window.location.href ="dash.html"
+    }) 
+})
